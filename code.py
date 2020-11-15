2d4f404c9e3c4a0bbae99c2f80c513a62bf5ea30
iguazio <iguazio@k8s-node1.cluster.local>
2020-11-15 15:47:23 +0200

    Add relative dir

--- code.py
+++ code.py
@@ -1,7 +1,7 @@
 import sys
-sys.path.append("/opt/nuclio/subdir/")
-import newcode
+from subdir import newcode
 
 def handler(context, event):
     val = newcode.newfunc()
     return "Hi from Edmond " + str(val)
+
